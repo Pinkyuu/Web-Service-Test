98339f4ff56683e87395bb0c76dba34b2f64b472
Pinkyuu <maksim7.25@mail.ru>
2023-09-11 12:20:08 +0700

    First commit, add get,post - request

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type item struct {
+	ID   int
+	Name string
+}
+
+var product = []item{
+	{ID: 1, Name: "Product 1"},
+	{ID: 2, Name: "Product 2"},
+	{ID: 3, Name: "Product 3"},
+}
+
+func main() {
+	http.HandleFunc("/product", personHandler)
+	http.HandleFunc("/health", healthCheckHandler)
+	log.Println("Server start listen port 8080!")
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func personHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getProduct(w, r)
+	case http.MethodPost:
+		postProduct(w, r)
+	case http.MethodPut:
+		putProduct(w, r)
+	default:
+		http.Error(w, "invalid http method", http.StatusMethodNotAllowed)
+	}
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(product)
+	fmt.Fprintf(w, "get product: '%v'", product)
+}
+
+func postProduct(w http.ResponseWriter, r *http.Request) {
+	var newProduct item
+	err := json.NewDecoder(r.Body).Decode(&newProduct)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	product = append(product, newProduct)
+	fmt.Fprintf(w, "post new product: '%v'", newProduct)
+}
+
+func putProduct(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "putProduct")
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Web-Service is working in normal mode! ")
+}
